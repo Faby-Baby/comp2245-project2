8e0369f43be23183ecb47d6e6e167e123599f1e7
Kalifa Roberts <kalifasroberts@gmail.com>
2022-12-04 15:38:52 -0400

    Created login page

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+
+<link rel="stylesheet" href="style.css" type="text/css">
+
+
+</head>
+<body>
+   
+    <form action="" method="post">
+
+
+    <input type="text" name="Username" id="username" placeholder = "Email Address">
+    <input type="password" name="Password" id="password" placeholder = "Password">
+    <button type="submit">Login</button>
+
+    </form>
+
+
+
+
+</body>
+</html>
