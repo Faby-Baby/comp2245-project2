dbebfea0cfaceaab0a1b2d77482831f6b806618f
Nandon <nandonarts@gmail.com>
2022-12-06 03:41:12 -0400

    Changes to the JavaScript Code

--- Inputs.js
+++ Inputs.js
@@ -1,49 +1,32 @@
 window.addEventListener('DOMContentLoaded',(event)=>{
-    /*
-    Note :How it loads in the information from the login to here, I need more info on.
-    
-    setting a system that inputs the content for:
-    Login .html
-    */
-    let Logbtn = document.querySelector( ".Log") 
-    const LInput =[]
-     LInput[0] = document.querySelector(".username")
-     LInput[1] = document.querySelector(".password")
-    let determinant = "http://localhost/comp2245-project2/login.php?user="//url  for the login
-    let extraparam = "&?password="
-    Logbtn.onclick = function(){
-    fetch(determinant + LInput[0].value + extraparam + LInput[1].value)// (might not work with form) trying out something weird with arrays 
-        .then (response =>  response.text())
-        .then(data =>{
-        console.log(data)// A method that triggers whether it goes to their page or gives an error it would probably require
-            //An alert or a inner html in red text can be used if the login is not validated
-    })
+
+//New Users
+    let Creatbtn = document.querySelector(".Uadd")
+    let firstname = document.querySelector(".fname")
+    let lastname = document.querySelector(".lname")
+    let emailaddress = document.querySelector(".email")
+    let password = document.querySelector(".password")
+    let Role = document.querySelector(".Role")
+    //sanitzation
+    Creatbtn.onclick = function()
+    {
+        fetch(`http://localhost/comp2245-project2/newuser.php?fname=${firstname}&?lname=${lastname}&?email=${emailaddress}&?password=${password}&?Role=${Role}`)
+        //post method
     }
-    //Contacts .html
+
+//Contacts
     let Conbtn = document.querySelector( ".CLog") //doesnt have a submit button
-    let ParaCon =["?Title=", "&?fname=","&?lname=","&?email=","&?phone=","&?company=","&?type=","&?Assigned=",]
-    const CIn=[]
-     CIn[0] = document.querySelector(".Title")
-     CIn[1] = document.querySelector(".firstname")
-     CIn[2] = document.querySelector(".lastname")
-     CIn[3] = document.querySelector(".emailadress")
-     CIn[4] = document.querySelector(".telephone")
-     CIN[5] = document.querySelector(".company")
-     CIN[6] = document.querySelector(".type")
-     CIN[7] = document.querySelector(".Assignto")
-    let Adder = "http://localhost/comp2245-project2/contact.php"//url  for the login
-    //for loop to make this string
-    let Arr = 0
-    for(ConIn of CIN){
-        Adder += ConIn.value + ParaCon[Arr]
-        Arr++
-    }
+     let Title = document.querySelector(".Title")
+     let fname = document.querySelector(".firstname")
+     let lname = document.querySelector(".lastname")
+     let email = document.querySelector(".emailadress")
+     let phone = document.querySelector(".telephone")
+     let company = document.querySelector(".company")
+     let type = document.querySelector(".type")
+     let Assignto = document.querySelector(".Assignto")
+     //Sanitization
     Conbtn.onclick = function(){
-    fetch(Adder)// +  whatever username (might not work with form) also gonna use a weird way for it hopefully a cleaner way is found before testing
-        .then (response =>  response.text())
-        .then(data =>{
-        console.log(data)// A method that triggers whether it goes to their page or gives an error it would probably require
-            //An alert or a inner html in red text can be used if the login is not validated
-    })
+    fetch(`http://localhost/comp2245-project2/contact.php?title=${Title}&?firstname=${fname}&?lname=${lname}&?email=${email}&?phone=${phone}&?company=${company}&?type=${type}&?Assignto=${Assignto}`)        .then (response =>  response.text())
+//post method
     }
     });
--- Loader.js
+++ Loader.js
@@ -5,25 +5,34 @@ Note :How it loads in the information from the login to here, I need more info o
 setting a system that loads the content for:
 Index
 */
-let Loader = document.querySelector( "#Refresher") //add a button with classname akin to Refresher to the html
+
+
+//push update 
+//Home
+//NewContact
+//User Admin only
+//New Users
+
+
+
 let table = document.querySelector(".Table") 
-let TableAcc = "?user="//url  for the tablr
-Loader.onclick = function(){
-fetch(TableAcc)// +  whatever username
+let user = ""
+let filter = ""//pushupate
+//url  for the tablr
+fetch(`?user=$` )// +  whatever username
     .then (response =>  response.text())
     .then(data =>{
     table.innerHTML= data 
     })
-}
+
 //Users(Admin Access)
-let ULoader = document.querySelector( "#Ufresher") //add a button with classname Refresher to the html
 let Utable = document.querySelector(".UserTable") 
 let  UTableAcc= "?admin="//which should it be user or admin
-ULoader.onclick = function(){
 fetch(UTableAcc)
     .then (response =>  response.text())
     .then(data =>{
     Utable.innerHTML= data 
     })
-}
+//Log out
+
 });
