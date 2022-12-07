e8f6725fec87f2936534a798c29baf1589f2e164
Nandon <nandonarts@gmail.com>
2022-12-06 22:11:24 -0400

    Addition of Files

--- login.php
+++ login.php
@@ -1,18 +1,49 @@
-<?php
-if(isset($_POST["button_login"]))          
-    $user   = $_POST["name"];
-    $pass   = $_POST["password"];
+<?php 
+session_start(); 
+include "db_conn.php";
 
-    $sql_login      = "SELECT * FROM user WHERE username='$user' AND password='$pass'";
-    $result_login   = $conn->query($sql_login);
-    $row_login      = $result_login->fetch_assoc();
-    $numrow_login   = $result_login->num_rows;
+if (isset($_POST['username']) && isset($_POST['password'])) {
 
-    if($numrow_login==1)
-    {
-        session_start();
-        $_SESSION['user'] = $user;
-        header('Location:index.php?');
-    }
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
 
-?>git 
+	$username = validate($_POST['username']);
+	$password = validate($_POST['password']);
+
+	if (empty($username)) {
+		header("Location: index.php?error=User Name is required");
+	    exit();
+	}else if(empty($password)){
+        header("Location: index.php?error=Password is required");
+	    exit();
+	}else{
+		$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+
+		$result = mysqli_query($conn, $sql);
+
+		if (mysqli_num_rows($result) === 1) {
+			$row = mysqli_fetch_assoc($result);
+            if ($row['username'] === $username && $row['password'] === $password) {
+            	$_SESSION['username'] = $row['username'];
+            	$_SESSION['name'] = $row['name'];
+            	$_SESSION['id'] = $row['id'];
+            	header("Location: dashboard.html");
+		        exit();
+            }else{
+				header("Location: index.php?error=Incorect User name or password");
+		        exit();
+			}
+		}else{
+			header("Location: index.php?error=Incorect User name or password");
+	        exit();
+		}
+	}
+	
+}else{
+	header("Location: index.php");
+	exit();
+}
